71e77c7a16359abdec6a8afe9191be1325c6015b
d33bs <dave.bunten@cuanschutz.edu>
2024-09-26 16:27:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 
 def main():
-    # make this VERY aggressive
-    print('Hello, World!!!!')
-    print('Hello again, world...')
     print('Hello again, world...?')
 
 if __name__ == '__main__':
